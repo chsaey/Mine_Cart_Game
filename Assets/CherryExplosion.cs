69cb0b15e2bf55a2b2054e9cd313a5d718816043
chsaey <chsaeydev@gmail.com>
2020-08-26 23:26:46 -0700

    Initial

---
+++ Assets/CherryExplosion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CherryExplosion : MonoBehaviour
+{
+    public int cherryValue = 1;
+    public bool taken = false;
+    public GameObject explosion;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && !taken) {
+            taken = true;
+
+            if (explosion)
+            {
+                Instantiate(explosion, transform.position, explosion.transform.rotation);
+            }
+            Object.Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Cart.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.Rendering;
+
+public class Cart : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    public float fallMultiplier;
+    public float lowJumpMultiplier;
+    public bool isGrounded = false;
+    Rigidbody2D _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        isGrounded = true;
+        _rigidbody.constraints = RigidbodyConstraints2D.None;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isGrounded = false;
+        _rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+        
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        _rigidbody.velocity = new Vector2(speed, _rigidbody.velocity.y);
+        
+        if (_rigidbody.velocity.y < 0)
+        {
+            _rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (_rigidbody.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            _rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
+        if (isGrounded && Input.GetButtonDown("Jump")) // If grounded AND jump button pressed, then allow the player to jump
+        {
+            // add a force in the up direction
+            _rigidbody.AddForce(new Vector2(0, jumpForce));
+        }
+
+    }
+}
